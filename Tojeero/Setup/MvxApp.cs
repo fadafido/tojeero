b74c9f1abc5f6e0f538d00abac8417f89b84e81c
Gagik Kyurkchyan <gagik.kyurkchyan@gmail.com>
2015-10-08 12:12:27 +0400

    Added hack to fix Parse request crash because of non-Gregorian calendar.

--- Tojeero/Setup/MvxApp.cs
+++ Tojeero/Setup/MvxApp.cs
@@ -4,6 +4,7 @@ using Cirrious.CrossCore;
 using Cirrious.MvvmCross;
 using Tojeero.Core;
 using System;
+using System.Globalization;
 
 namespace Tojeero
 {
@@ -38,6 +39,10 @@ namespace Tojeero
 				.RegisterAsDynamic();
 
 			Mvx.LazyConstructAndRegisterSingleton<ILogger, Logger>();
+			//We need to set thread culture to english because if the device culture will be arabic
+			//when sending requests to parse.com app will use arabic calender, which cause app crash.
+			//This will insure that all requests sent to Parse.com will have english culture and thus Gregorian calendar.
+			CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en");
 			RegisterAppStart(new EmptyStart());
         }
     }

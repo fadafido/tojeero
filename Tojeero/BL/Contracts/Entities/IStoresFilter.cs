68dffee85ffee022f6b62e15a50a1dc82db94f77
Gagik Kyurkchyan <gagik.kyurkchyan@gmail.com>
2015-10-21 10:50:46 +0400

    Added StoreFilter class.

---
+++ Tojeero/BL/Contracts/Entities/IStoresFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Tojeero.Core
+{
+	public interface IStoreFilter : INotifyPropertyChanged
+	{
+		IStoreCategory Category { get; set; }
+		ICountry Country { get; set; }
+		ICity City { get; set; }
+		ObservableCollection<string> Tags { get; set; }
+		IProductFilter Clone();
+	}
+}
+
